ef6ea2ad4440d4d64e3df07c1bf929d28bbb0fe5
pandafuture <2633024197@qq.com>
2025-07-03 01:51:23 +0800

    第一次提交

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private string str = "First";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
